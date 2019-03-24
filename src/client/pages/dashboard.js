bbb0a6c80d365ca00f5a2b45fbf23bd730757baf
DennisOnder <webdev.dennisonder@gmail.com>
2019-03-24 20:54:39 +0100

    Dependencies, more dashboard tweaks

--- src/client/pages/dashboard.js
+++ src/client/pages/dashboard.js
@@ -7,7 +7,7 @@ const Sidebar = () => {
   return (
     <div className={css.sidebar}>
       <ul>
-        <li><i className="fas fa-plus-circle fa-2x"></i></li>
+        <li><i className="fas fa-plus-circle fa-2x" onClick={Store.addFieldToVault}></i></li>
         <li><i className="fas fa-cog fa-2x"></i></li>
         <li><i className="fas fa-sign-out-alt fa-2x" onClick={Store.logoutUser}></i></li>
       </ul>
@@ -16,16 +16,35 @@ const Sidebar = () => {
 };
 
 export default class Dashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      vaultOpen: false
+    };
+  }
   componentDidMount = () => {
     Store.checkForVault();
+    if (Store.store.vaultExists) {
+      Store.openVault();
+    }
   }
   render() {
     return (
-      <div className={css.dashboard}>
-        <Head title="Sentinel - Dashboard" />
-        <div>
-          <Sidebar />
-          <h1>Dashboard</h1>
+      <div>
+        <Sidebar />
+        <div className={css.dashboard}>
+          <Head title="Sentinel - Dashboard" />
+          <div>
+            <div className={css.wrapper}>
+              <h1>Dashboard</h1>
+              <div className={css.output} style={{ display: `${this.state.vaultOpen ? 'block' : 'none'}` }}>
+                <div className={css.field}><p>Name: Test</p><input type="password" placeholder="Password:" /></div>
+                <div className={css.field}><p>Name: Test</p><input type="password" placeholder="Password:" /></div>
+                <div className={css.field}><p>Name: Test</p><input type="password" placeholder="Password:" /></div>
+                <div className={css.field}><p>Name: Test</p><input type="password" placeholder="Password:" /></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
